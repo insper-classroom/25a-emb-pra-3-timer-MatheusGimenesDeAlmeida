37c6b91d3394d7cb3c059b720e1ea3d784e09eaf
MatheusGimenesDeAlmeida <mthga500@gmail.com>
2025-03-20 15:24:14 -0300

    final

--- main/main.c
+++ main/main.c
@@ -9,7 +9,6 @@
  #define TRIGGER_PIN 5
  #define ECHO_PIN    15
 
-
  #define TIMEOUT_MS  50
  
  typedef struct {
@@ -21,63 +20,64 @@
  } sensor_data_t;
  
  volatile static sensor_data_t sensor_data = {0};
+ volatile bool flag_fail;
  
- int callback_timeout(alarm_id_t id, void *user_data) {
-     if (sensor_data.aguarda_pulso) {
-         datetime_t agora;
-         rtc_get_datetime(&agora);
-        
-         printf("%02d:%02d:%02d - Erro: pulso não recebido\n", agora.hour, agora.min, agora.sec);
-         sensor_data.aguarda_pulso = false;
-     }
-     return 0; 
- }
- 
- void callback_echo(uint gpio, uint32_t eventos) {
-     if (eventos & GPIO_IRQ_EDGE_RISE) {
-         sensor_data.inicio = get_absolute_time();
-         if (sensor_data.aguarda_pulso) {
-             cancel_alarm(sensor_data.alarm);
-             sensor_data.aguarda_pulso = false;
-         }
-     } else if (eventos & GPIO_IRQ_EDGE_FALL) {
-         sensor_data.fim = get_absolute_time();
-         sensor_data.pulso_fall = 1;
-     }
- }
+
+ void echo_callback(uint gpio, uint32_t events) {
+    if (events & GPIO_IRQ_EDGE_RISE) {
+        sensor_data.inicio = get_absolute_time();
+        if (sensor_data.aguarda_pulso) {
+            cancel_alarm(sensor_data.alarm);
+            sensor_data.aguarda_pulso = false;
+        }
+    } if (events & GPIO_IRQ_EDGE_FALL) {
+        sensor_data.fim = get_absolute_time();
+        sensor_data.pulso_fall = 1;
+    }
+}
+int64_t alarme_timer(alarm_id_t id, void *user_data) {
+    if (sensor_data.aguarda_pulso) {
+        datetime_t agora;
+        rtc_get_datetime(&agora);
+        printf("%02d:%02d:%02d - Erro: pulso não recebido\n", agora.hour, agora.min, agora.sec);
+        sensor_data.aguarda_pulso = false;
+        flag_fail = true;
+    }
+    return 0;  
+}
  
  int main() {
+    rtc_init();
      stdio_init_all();
- 
-     datetime_t tempo_inicial = {
-         .year  = 2025,
-         .month = 3,
-         .day   = 18,
-         .dotw  = 0,   
-         .hour  = 0,
-         .min   = 0,
-         .sec   = 0
-     };
-     rtc_init();
+      datetime_t tempo_inicial = {
+        .year  = 2025,
+        .month = 3,
+        .day   = 18,
+        .dotw  = 0,   
+        .hour  = 0,
+        .min   = 0,
+        .sec   = 0
+    };
+
      rtc_set_datetime(&tempo_inicial);
+
  
-     gpio_init(TRIGGER_PIN);
-     gpio_set_dir(TRIGGER_PIN, GPIO_OUT);
- 
+     // Configura os pinos do sensor
      gpio_init(ECHO_PIN);
      gpio_set_dir(ECHO_PIN, GPIO_IN);
+     gpio_init(TRIGGER_PIN);
+     gpio_set_dir(TRIGGER_PIN, GPIO_OUT);
      gpio_set_irq_enabled_with_callback(ECHO_PIN,
                                         GPIO_IRQ_EDGE_RISE | GPIO_IRQ_EDGE_FALL,
                                         true,
-                                        callback_echo);
- 
-     bool medindo = false;
- 
+                                        echo_callback);
+    bool medindo = false;
+  
      while (true) {
-         int ch = getchar_timeout_us(500);
-         if (ch == 'm') {
+         int ch = getchar_timeout_us(300);
+         if (ch == 's') {
              medindo = !medindo;
-             if (medindo) {
+             if (medindo == true) {
                  printf("Medições iniciadas\n");
              } else {
                  printf("Medições pausadas\n");
@@ -85,23 +85,36 @@
          }
  
          if (medindo) {
+
              gpio_put(TRIGGER_PIN, 1);
              sleep_us(10);
              gpio_put(TRIGGER_PIN, 0);
  
              sensor_data.aguarda_pulso = true;
-             sensor_data.alarm = add_alarm_in_ms(TIMEOUT_MS, callback_timeout, NULL, false);
+             sensor_data.alarm = add_alarm_in_ms(TIMEOUT_MS, alarme_timer, NULL, false);
+
+             if (flag_fail){
+                printf("Não leu");
+                flag_fail = false;
+             }
  
              if (sensor_data.pulso_fall == 1) {
-                 sensor_data.pulso_fall = 0;
+
                  int64_t delta_us = absolute_time_diff_us(sensor_data.inicio, sensor_data.fim);
-                 double distancia = (delta_us * 0.0343) / 2.0;  
+
+                 double distancia_total = (delta_us * 0.0343/2); 
+                 sensor_data.pulso_fall = !sensor_data.pulso_fall;
+                 if (distancia_total >= 400){
+                    printf("Não leu\n");
+                 }
+                else{
  
                  datetime_t agora;
                  rtc_get_datetime(&agora);
                  char buffer_data[256];
                  datetime_to_str(buffer_data, sizeof(buffer_data), &agora);
-                 printf("%s - Distância: %.2f cm\n", buffer_data, distancia);
+
+                 printf("%s - Distância: %.2f cm\n", buffer_data, distancia_total);}
              }
          }
  
